f26d0ec9192ea4c654dae3ed5c338545e6ad4dc7
yankee <yankee@debian>
2016-12-10 20:28:57 +0545

    Sum numbers

--- find_duplicates.rb
+++ find_duplicates.rb
@@ -4,3 +4,4 @@ def unique(integers)
 		yankee<<i unless yankee.include?(i)
 	end
 end
+unique([1,2,3,4,5,6])
---
+++ sum_numbers.rb
@@ -0,0 +1,6 @@
+# Sum Numbers
+def sum(numbers)
+	sum=0
+	numbers.each {|i| sum+=i}
+	return sum 
+end
