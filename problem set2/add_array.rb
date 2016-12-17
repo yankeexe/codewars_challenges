49811e1ae521e732ecab7c8b6cdbd648cd165656
yankee <yankee@debian>
2016-12-17 13:19:47 +0545

    add_array

---
+++ problem set2/add_array.rb
@@ -0,0 +1,9 @@
+# add the value "codewars" to the already defined websites array
+websites<<("codewars")
+
+#test
+	describe "Solution" do
+  it "should test for something" do
+    Test.assert_equals(websites[0], "codewars", "This is just an example of how you can write your own TDD tests")
+  end
+end
---
+++ problem set2/tip_calculator.rb
@@ -0,0 +1,7 @@
+def calculate_tips(amount, rating)
+	puts "Enter the amount"
+	amount=gets.chomp.to_i
+	puts "Enter the tip percentage: 0,5,10,15,20"
+	tips=gets.chomp.to_i		 
+end
+	
