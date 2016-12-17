469528644a9795a4fdf40caad07030bcd1a88d6a
yankee <yankee@debian>
2016-12-17 12:39:48 +0545

    Grader

---
+++ problem set1/grader.rb
@@ -0,0 +1,12 @@
+def grader(score)
+  # TODO: complete me
+  return "F" if (score>1||score<0.6)
+  return "A" if (score>=0.9)
+  return "B" if (score>=0.8)
+  return "C" if (score>=0.7)
+  return "D" if (score>=0.6)
+end
+
+Test.assert_equals(grader(0.7), "C")
+Test.assert_equals(grader(0.9), "A")
+Test.assert_equals(grader(0.6),  "D")
